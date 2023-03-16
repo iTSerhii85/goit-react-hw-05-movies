a8b238276b2f04ef0dcb9b5f4cfe1aefadac5f0e
iTSerhii85 <109918837+iTSerhii85@users.noreply.github.com>
2023-03-16 15:41:04 +0000

    Deploying to gh-pages from @ iTSerhii85/goit-react-hw-05-movies@fd61884d00140c87be0fc999b4ebddf65cb65ca5 🚀

---
+++ static/js/23.4d803a33.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[23],{23:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h2",{children:"Reviews"})}}}]);
+//# sourceMappingURL=23.4d803a33.chunk.js.map
