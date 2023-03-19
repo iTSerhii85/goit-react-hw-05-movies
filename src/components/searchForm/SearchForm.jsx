fbd2b556aa373a767140ba2a4e52f483d78756b4
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2023-03-19 21:52:08 +0200

    made skeleton

--- src/components/searchForm/SearchForm.jsx
+++ src/components/searchForm/SearchForm.jsx
@@ -1,25 +1,54 @@
 import { useState } from 'react';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const SearchForm = ({ onSubmit, placeholder }) => {
   const [searchInputValue, setSearchInputValue] = useState('');
 
   const handleSubmit = evt => {
     evt.preventDefault();
+    if (searchInputValue === '') {
+      toast.warn('Enter movie name!', {
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+    }
     onSubmit(searchInputValue);
     setSearchInputValue('');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        autoComplete="off"
-        autoFocus
-        placeholder={placeholder === '' ? 'Enter movie name' : placeholder}
-        value={searchInputValue}
-        onChange={e => setSearchInputValue(e.target.value)}
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder={placeholder === '' ? 'Enter movie name' : placeholder}
+          value={searchInputValue}
+          onChange={e => setSearchInputValue(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
       />
-      <button type="submit">Search</button>
-    </form>
+    </>
   );
 };
--- src/pages/movieDetails/MovieDetails.jsx
+++ src/pages/movieDetails/MovieDetails.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { NavLink, Outlet, useParams } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Wrapper } from './MovieDetails.styled';
+import ContentLoader from 'react-content-loader';
 
 const setActive = ({ isActive }) =>
   isActive
@@ -25,7 +26,27 @@ const MovieDetails = () => {
 
   return (
     <main>
-      {movie && <AboutMovie movie={movie} />}
+      {!movie ? (
+        <ContentLoader
+          speed={1}
+          width={1240}
+          height={380}
+          viewBox="0 0 1240 380"
+          backgroundColor="#e0eaff"
+          foregroundColor="#ecebeb"
+        >
+          <rect x="243" y="48" rx="3" ry="3" width="225" height="15" />
+          <rect x="247" y="139" rx="3" ry="3" width="285" height="10" />
+          <rect x="8" y="12" rx="0" ry="0" width="216" height="354" />
+          <rect x="248" y="198" rx="3" ry="3" width="150" height="15" />
+          <rect x="246" y="116" rx="3" ry="3" width="285" height="10" />
+          <rect x="247" y="279" rx="3" ry="3" width="285" height="10" />
+          <rect x="246" y="259" rx="3" ry="3" width="285" height="10" />
+          <rect x="247" y="238" rx="3" ry="3" width="285" height="10" />
+        </ContentLoader>
+      ) : (
+        <AboutMovie movie={movie} />
+      )}
       <Wrapper>
         <h3>Additional information:</h3>
         <NavLink to="cast" style={setActive}>
